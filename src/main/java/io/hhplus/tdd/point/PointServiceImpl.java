c9abf34f714da0a573b58ca909bfa54f73f5da2f
DK2554 <ldu0215@gmail.com>
2024-12-19 19:54:18 +0900

    # 포인트충전과 포인트사용 동시성 제어 기능 구현 + 동시성제어 통합테스트 코드 작성

--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -5,6 +5,7 @@ import io.hhplus.tdd.database.UserPointTable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class PointServiceImpl implements PointService{
@@ -15,6 +16,8 @@ public class PointServiceImpl implements PointService{
     //최소금액
     private static final long MIN_AMOUNT = 0L;
 
+    private final ReentrantLock lock = new ReentrantLock(true);
+
     public PointServiceImpl(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
         this.userPointTable = userPointTable;
         this.pointHistoryTable = pointHistoryTable;
@@ -22,24 +25,28 @@ public class PointServiceImpl implements PointService{
 
     @Override
     public UserPoint chargeUserPoint(long userId, long amount) {
+        lock.lock(); // 락 획득
+        try{
+            UserPoint userPoint = userPointTable.selectById(userId);
 
-        UserPoint userPoint = userPointTable.selectById(userId);
+            if(amount <= MIN_AMOUNT){
+                throw new IllegalArgumentException("충전금액으 0보다 커야합니다.");
+            }
 
-        if(amount <= MIN_AMOUNT){
-            throw new IllegalArgumentException("충전금액으 0보다 커야합니다.");
+            long updateAmount = userPoint.point() + amount;
+            // 최대 잔고 초과 여부 확인
+            if (updateAmount > MAX_AMOUNT) {
+                throw new IllegalArgumentException("잔고가 초과 되었습니다 포인트는 " + MAX_AMOUNT + "을 초과할 수 없습니다.");
+            }
+
+            UserPoint updateUser =  userPointTable.insertOrUpdate(userId, updateAmount);
+
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+            return updateUser;
+        }finally {
+            lock.unlock();
         }
-
-        long updateAmount = userPoint.point() + amount;
-        // 최대 잔고 초과 여부 확인
-        if (updateAmount > MAX_AMOUNT) {
-            throw new IllegalArgumentException("잔고가 초과 되었습니다 포인트는 " + MAX_AMOUNT + "을 초과할 수 없습니다.");
-        }
-
-        UserPoint updateUser =  userPointTable.insertOrUpdate(userId, amount);
-
-        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
-
-        return updateUser;
     }
 
     @Override
@@ -64,25 +71,30 @@ public class PointServiceImpl implements PointService{
 
     @Override
     public UserPoint useUserPoint(long userId, long amount) {
+        lock.lock();
+        try{
+            UserPoint userPoint = userPointTable.selectById(userId);
 
-        UserPoint userPoint = userPointTable.selectById(userId);
+            if(amount <= MIN_AMOUNT) {
+                throw new IllegalArgumentException("사용금액은 0보다 커야합니다.");
+            }
 
-        if(amount <= MIN_AMOUNT) {
-            throw new IllegalArgumentException("사용금액은 0보다 커야합니다.");
+            long updateAmount = userPoint.point() - amount;
+            // 최대 잔고 초과 여부 확인
+
+            if(updateAmount < 0){
+                throw new IllegalArgumentException("잔고에 금액이 부족합니다");
+            }
+
+            UserPoint updateUser =  userPointTable.insertOrUpdate(userId, updateAmount);
+
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+            return updateUser;
+        }finally {
+            lock.unlock();
         }
 
-        long updateAmount = userPoint.point() - amount;
-        // 최대 잔고 초과 여부 확인
-
-        if(updateAmount < 0){
-            throw new IllegalArgumentException("잔고에 금액이 부족합니다");
-        }
-
-        UserPoint updateUser =  userPointTable.insertOrUpdate(userId, updateAmount);
-
-        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
-
-        return updateUser;
     }
 
 
